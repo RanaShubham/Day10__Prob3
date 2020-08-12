8918fe14db0661c8141b84401253b43b4b3bfd08
Shubham Rana <ranashubham7296@gmail.com>
2020-08-12 22:42:23 +0530

    Refactored flipCoin_UC1 to take number of flips from user and show percentage of head or tails show up

--- flipCoinCombination.sh
+++ flipCoinCombination.sh
@@ -2,4 +2,39 @@
 
 echo "Welcome to flip coin combination program"
 
-echo $((RANDOM%2))
+read -p "How many times do you want to flip coin: " times
+
+declare -A coin
+counter=0
+heads=0
+tails=0
+
+while [[ $counter != $times ]]
+do
+	face=$((RANDOM%2))
+
+	if [[ $face = 1 ]]
+	then
+		((heads++))
+	else
+		((tails++))
+	fi
+
+	((counter++))
+done
+
+coin[H]=$heads
+coin[T]=$tail
+
+function facePercent ()
+{
+	local faceTimes=$1
+	local total=$2
+
+	per=`awk " BEGIN {print ($faceTimes/$total)*100 }"`
+	echo "$per"
+}
+
+#Using subshell to run the function
+echo "heads showed up $(facePercent $heads $times)% "
+echo "tails showed up $(facePercent $tails $times)% "
